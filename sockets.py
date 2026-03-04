2f3fd5a8f4e3ca0c48d738035b7554aaee047a61
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-05 01:58:29 +0900

    feat: 웹소켓 이벤트 헨들러 구현

---
+++ sockets.py
@@ -0,0 +1,33 @@
+# sockets.py
+from flask import request
+from flask_socketio import ConnectionRefusedError, join_room
+from flask_jwt_extended import decode_token
+from extensions import socketio
+
+@socketio.on('connect')
+def handle_connect():
+    """
+    연결 및 인증 (Handshake)
+    클라이언트가 연결을 시도할 때 쿠키에 있는 JWT를 검사합니다.
+    """
+    token = request.cookies.get('access_token_cookie')
+    
+    if not token:
+        raise ConnectionRefusedError('authentication_required')
+    
+    try:
+        decode_token(token)
+        print("웹소켓 인증 성공 및 연결됨!")
+    except Exception:
+        raise ConnectionRefusedError('authentication_required')
+
+@socketio.on('join_board')
+def on_join_board(data):
+    """
+    클라이언트가 연결 후 특정 보드(칠판) 방에 입장시켜 달라고 요청할 때 실행됩니다.
+    """
+    public_id = data.get('public_id')
+    if public_id:
+        room_name = f"board_{public_id}"
+        join_room(room_name)
+        print(f"[{room_name}] 방에 유저가 입장했습니다.")
