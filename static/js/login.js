753dd43f382f11a9f6c1d634b5d1658b5931b755
HyeYeonLee0112 <139879153+Azebalue@users.noreply.github.com>
2026-03-04 21:40:05 +0900

    feat: 로그인 ui

---
+++ static/js/login.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    
+    // 로그인 처리
+    $('[type="submit"]').click(function(e) {
+
+        //e.preventDefault();
+
+        const id = $('#id').val();
+        const pw = $('#password').val();
+
+        // $.ajax를 통해 /api/auth/login 호출
+        
+
+
+    });
+
+    // 회원가입 처리
+    $('#register').click(function(e) {
+
+    });
+});
