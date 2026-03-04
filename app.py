d5ecbb5e66dafc49ad24189a4a367ecec18e6611
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-04 19:57:31 +0900

    db: 기본 연결 추가

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+#!!! 
+# 로컬 db 설정에 맞춰 수정 필요
+client = MongoClient('mongodb://localhost:27017')
+db = client["mydb"]
 
 @app.route('/')
 def home():
