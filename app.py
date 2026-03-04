2fd4cdff211b84deb577e384fc1d306302c058a2
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-04 22:26:16 +0900

    feat: 로그인 컨트롤러 구현

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
-from services import register_user
+from config import Config
+from services import register_user, login_user
+from flask_jwt_extended import JWTManager, create_access_token, set_access_cookies 
+from datetime import timedelta
 
 app = Flask(__name__)
 CORS(app)
 
+app.config["JWT_SECRET_KEY"] = Config.SECRET_KEY
+app.config["JWT_TOKEN_LOCATION"] = ['cookies']
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+jwt = JWTManager(app)
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# 일단은 access token만 httponly로 발급 시간이 된다면 refresh token도 발급
+@app.route('/api/auth/login', methods=['POST'])
+def api_login():
+    data = request.get_json()
+    
+    if not data:
+      return jsonify({"error": "요청 본문(Body)이 비어있습니다."}), 422
+
+    user_data, statuscode = login_user(data)
+    
+    if statuscode != 200:
+      return jsonify(user_data), statuscode
+
+    access_token = create_access_token(identity=user_data['user_id'])
+
+    response = jsonify({
+      "user_id": user_data['user_id'],
+      "username": user_data['username']
+    })
+
+    set_access_cookies(response, access_token)
+
+    return response, statuscode
 
 
 @app.route('/api/auth/register', methods=['POST'])
