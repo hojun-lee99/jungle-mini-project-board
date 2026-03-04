233348207abbf733296851dd462a45915a8eb7ee
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-04 20:58:53 +0900

    feat: 회원가입 컨트롤러 구현
    db client 생성 코드를 service.py로 옮겼음

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template
-from pymongo import MongoClient
+from flask import Flask, render_template, jsonify, request
+from services import register_user
 
 app = Flask(__name__)
 
-#!!! 
-# 로컬 db 설정에 맞춰 수정 필요
-client = MongoClient('mongodb://localhost:27017')
-db = client["mydb"]
-
 @app.route('/')
 def home():
-  return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/api/auth/register', methods=['POST'])
+def api_register():
+    data = request.get_json()
+
+    if not data:
+      return jsonify({"error": "요청 본문(Body)이 비어있습니다."}), 422
+    
+    response_data, statuscode = register_user(data)
+
+    return jsonify(response_data), statuscode
 
 if __name__ == '__main__':  
   app.run('0.0.0.0', port=5001, debug=True)
